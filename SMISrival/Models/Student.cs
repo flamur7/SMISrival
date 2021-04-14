a6dbc31edf6aca6abd3013d00284f67ccdb3ce7d
flamur7 <flamur.ga7@gmail.com>
2021-04-14 19:57:39 +0200

    Atribute of Student

--- SMISrival/Models/Student.cs
+++ SMISrival/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace SMISrival.Models
 {
     public class Student
     {
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Surname { get; set; }
+        public String BirthPlace { get; set; }
+        public int Semester { get; set; }
+        public char Gender { get; set; }
+        [Required]
+        public String PersonalId { get; set; }
+
     }
 }
