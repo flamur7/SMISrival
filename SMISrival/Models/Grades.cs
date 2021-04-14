bbc2eacce6d9315fb79d6547d778d4b78ee4e3cf
flamur7 <flamur.ga7@gmail.com>
2021-04-14 21:06:17 +0200

    Grade Attributes

--- SMISrival/Models/Grades.cs
+++ SMISrival/Models/Grades.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace SMISrival.Models
 {
     public class Grades
     {
+        public int Id { get; set; }
+        [Required]
+        public String PersonalId { get; set; }
+        [Required]
+        public String CodeTeacher { get; set; }
+        [Required]
+        public String Subjects { get; set; }
+        [Required]
+        public String Grade { get; set; }
+
+
+
     }
 }
