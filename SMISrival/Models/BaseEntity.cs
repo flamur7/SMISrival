c09674497751e9710e3a31c3d2367d0240007d47
flamur7 <flamur.ga7@gmail.com>
2021-04-14 21:13:49 +0200

    BaseEntity Attribute

--- SMISrival/Models/BaseEntity.cs
+++ SMISrival/Models/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace SMISrival.Models
 {
     public class BaseEntity
     {
+        [DisplayName("Created By")]
+
+        public String CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public String UpdateBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
