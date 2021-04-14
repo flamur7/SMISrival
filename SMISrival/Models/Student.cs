41774c8352399058d2126405a4d6d799308423ab
flamur7 <flamur.ga7@gmail.com>
2021-04-14 20:41:24 +0200

    Fixed attributes

--- SMISrival/Models/Student.cs
+++ SMISrival/Models/Student.cs
@@ -8,16 +8,19 @@ namespace SMISrival.Models
 {
     public class Student
     {
+        [Key]
         public int Id { get; set; }
         [Required]
         public String Name { get; set; }
+        public String ParentName { get; set; }
         [Required]
         public String Surname { get; set; }
-        public String BirthPlace { get; set; }
-        public int Semester { get; set; }
         public char Gender { get; set; }
+        [Required] 
+        public String Branch { get; set; }
         [Required]
         public String PersonalId { get; set; }
+        public int Semester { get; set; }
 
     }
-}
+} 
