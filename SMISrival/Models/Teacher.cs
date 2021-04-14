08fc055218a1b8fe60c6bd0d39b09e6b004ebfb3
flamur7 <flamur.ga7@gmail.com>
2021-04-14 20:54:01 +0200

    Teacher Attribute

--- SMISrival/Models/Teacher.cs
+++ SMISrival/Models/Teacher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,22 @@ namespace SMISrival.Models
 {
     public class Teacher
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Surname { get; set; }
+        [Required]
+        public String Branch { get; set; }
+
+        [Required]
+        public String Subject { get; set; }
+        [Required]
+        public String TeacherCode { get; set; }
+
+        public String University { get; set; }
+
+        public String Title { get; set; }
     }
 }
