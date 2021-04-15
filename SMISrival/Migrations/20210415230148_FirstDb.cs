3f874e073a1ef7a1892630bdaf0afee13a26e79c
flamur7 <flamur.ga7@gmail.com>
2021-04-16 01:13:23 +0200

    Connection/Migration with database

---
+++ SMISrival/Migrations/20210415230148_FirstDb.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SMISrival.Migrations
+{
+    public partial class FirstDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonalId = table.Column<string>(nullable: false),
+                    CodeTeacher = table.Column<string>(nullable: false),
+                    Subjects = table.Column<string>(nullable: false),
+                    Grade = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    ParentName = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: false),
+                    Gender = table.Column<string>(nullable: false),
+                    Branch = table.Column<string>(nullable: false),
+                    PersonalId = table.Column<string>(nullable: false),
+                    Semester = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Surname = table.Column<string>(nullable: false),
+                    Branch = table.Column<string>(nullable: false),
+                    Subject = table.Column<string>(nullable: false),
+                    TeacherCode = table.Column<string>(nullable: false),
+                    University = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grades");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
