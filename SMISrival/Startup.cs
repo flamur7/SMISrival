a7ca1597c52380bc265c0ad541f0fbece0326e1d
flamur7 <flamur.ga7@gmail.com>
2021-04-16 00:54:55 +0200

    Creating the service

--- SMISrival/Startup.cs
+++ SMISrival/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SMISrival.Areas.Identity.Data;
+using SMISrival.Data;
 using SMISrival.Models;
 using System;
 using System.Collections.Generic;
@@ -28,6 +31,8 @@ namespace SMISrival
             services.AddDbContext<ApplicationDbContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("ApplicationDbContextConnection"))
                 );
+            services.AddDefaultIdentity<AuthUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AuthDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
