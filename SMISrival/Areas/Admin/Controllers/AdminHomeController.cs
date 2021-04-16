6fe469b3b0897b0a54a2bca1d2a0024765c62cf6
flamur7 <flamur.ga7@gmail.com>
2021-04-16 02:49:31 +0200

    Add Admin Controller

---
+++ SMISrival/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMISrival.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminHomeController : Controller
+    {
+
+
+        private readonly RoleManager<IdentityRole> rolemanager;
+        public AdminHomeController(RoleManager<IdentityRole> _roleManager)
+        {
+            rolemanager = _roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+
+            if (!await rolemanager.RoleExistsAsync(roleName))
+            {
+
+                await rolemanager.CreateAsync(new IdentityRole(roleName));
+            }
+
+            return View();
+        }
+    }
+}
