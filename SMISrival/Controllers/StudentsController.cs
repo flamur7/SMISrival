d62bea9bc1be12535def76f7343160e33b596d72
flamur7 <flamur.ga7@gmail.com>
2021-04-16 03:33:28 +0200

    Logic of Student Controller

--- SMISrival/Controllers/StudentsController.cs
+++ SMISrival/Controllers/StudentsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SMISrival.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +9,117 @@ using System.Threading.Tasks;
 namespace SMISrival.Controllers
 {
     public class StudentsController : Controller
-    {
+    {  
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Student Studnet { get; set; }
+        public StudentsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Upsert(int? id)
+        {
+            Studnet = new Student();
+            if (id == null)
+            {
+                //create
+                return View(Studnet);
+            }
+            //update
+            Studnet = _db.Students.FirstOrDefault(u => u.Id == id);
+            if (Studnet == null)
+            {
+                return NotFound();
+            }
+            return View(Studnet);
+        }
+
+        public IActionResult Info(int? id)
+        {
+            Studnet = new Student();
+            if (id == null)
+            {
+                //create
+                return View(Studnet);
+            }
+            //update
+            Studnet = _db.Students.FirstOrDefault(u => u.Id == id);
+            if (Studnet == null)
+            {
+                return NotFound();
+            }
+            return View(Studnet);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Studnet.Id == 0)
+                {
+                    //create
+                    _db.Students.Add(Studnet);
+                }
+                else
+                {
+                    _db.Students.Update(Studnet);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Studnet);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Info()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Studnet.Id == 0)
+                {
+                    //create
+                    _db.Students.Add(Studnet);
+                }
+                else
+                {
+                    _db.Students.Update(Studnet);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Studnet);
+        }
+
+
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Students.ToListAsync() });
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var studentFromDb = await _db.Students.FirstOrDefaultAsync(u => u.Id == id);
+            if (studentFromDb == null)
+            {
+                return Json(new { success = false, message = "Error" });
+            }
+            _db.Students.Remove(studentFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Sukseshem" });
+        }
+        #endregion
     }
 }
